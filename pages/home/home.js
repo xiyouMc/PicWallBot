c5c468e5b18a85a397b963709bb92fbe57c22f1a
XiyouMc <xiyoumc.dev@gmail.com>
2018-07-19 01:41:29 +0800

    重构页面，支持新的Api 接口， 同时支持 Video 、分享等功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,22 +2,27 @@
 var api = require('../../utils/request_api.js')
 var defaultData = '<xml><URL><![CDATA[https://python.0x2048.com/wx/]]></URL><ToUserName><![CDATA[mm]]></ToUserName><FromUserName><![CDATA[aa]]></FromUserName><CreateTime>111</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[##]]></Content><MsgId>123</MsgId></xml>'
 var avatarUrl = ''
-var picUrl = []
+var avatarName = ''
+var dataUrls = []
 var inputUrl = ''
+var isVideo = false
+// var videoUrl = ''
+var saveTip = '保存到相册'
+var isShowSave = true
+var onLoadInputUrl = ''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isHidePic: false,
+    isHidePic: true,
     avatarUrl: avatarUrl,
-    picUrl: picUrl,
-    avatarName: '',
-    inputUrl: '',
-    videoUrl: '',
-    //图片地址
-    imgList: [],
+    avatarName: avatarName,
+    inputUrl: inputUrl,
+    //图片 或者视频地址
+    dataUrls: dataUrls,
+    // videoUrls: videoUrls,
     //是否采用衔接滑动  
     circular: false,
     //是否显示画板指示点  
@@ -37,14 +42,21 @@ Page({
     //图片宽度 
     imgwidth: 750,
     //默认  
-    current: 0
+    current: 0,
+    saveTip: saveTip,
+    isShowSave: isShowSave,
+    needShowSwipe: false,
+    needShowVideo: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    console.log('fdsdsfsdf')
+    console.log(options.inputUrl)
+    onLoadInputUrl = options.inputUrl
+    inputUrl = ''
   },
 
   /**
@@ -59,17 +71,24 @@ Page({
    */
   onShow: function() {
     var that = this
-    wx.getClipboardData({
-      success: function(res) {
-        if (res.data.indexOf('instagram.com') != -1 && inputUrl != res.data) {
-          inputUrl = res.data
-          that.setData({
-            insUrl: res.data
-          })
-          that.requestPic(res.data)
+    if (onLoadInputUrl !== null && onLoadInputUrl !== undefined && onLoadInputUrl !== '') {
+      that.setData({
+        insUrl: onLoadInputUrl
+      })
+      that.requestPic(onLoadInputUrl)
+    } else {
+      wx.getClipboardData({
+        success: function(res) {
+          if (res.data.indexOf('instagram.com') != -1 && inputUrl != res.data) {
+
+            that.setData({
+              insUrl: res.data
+            })
+            that.requestPic(res.data)
+          }
         }
-      }
-    })
+      })
+    }
   },
 
   /**
@@ -108,9 +127,18 @@ Page({
   },
 
   requestPic: function(url) {
+    if (inputUrl === url) {
+      return
+    }
+    inputUrl = url
+    this.setData({
+      needShowSwipe: false,
+      needShowVideo: false
+    })
     wx.showLoading({
       title: '正在爬取中。。。',
     })
+    dataUrls = []
     var that = this
     wx.request({
       url: api.serverApi,
@@ -124,50 +152,145 @@ Page({
           // var reg = <Title><[CDATA[(/.*)]\.*<![CDATA[(\.*)><\/PicUrl>\.*<![CDATA[(.*)></Description>.*<![CDATA[(.*)>;
           // reg.exec(e.data)
           console.log(e.data)
-          avatarUrl = e.data.avatar_url
-          picUrl = e.data.imgsBase64;
-          console.log(picUrl)
+          var shortcodeMedia = e.data.graphql.shortcode_media
+          var owner = shortcodeMedia.owner
+
+          avatarUrl = owner.profile_pic_url
+          avatarName = owner.username
+          if (shortcodeMedia.edge_sidecar_to_children) {
+            for (var index = 0; index < shortcodeMedia.edge_sidecar_to_children.edges.length; index++) {
+              var node = shortcodeMedia.edge_sidecar_to_children.edges[index].node
+              var isVideo = node.is_video
+              if (!isVideo) {
+                that.setData({
+                  needShowSwipe: true
+                })
+              } else {
+                that.setData({
+                  needShowVideo: true
+                })
+              }
+              dataUrls.push({
+                "src": isVideo ? node.video_url : node.display_resources[1].src,
+                "isVideo": isVideo
+              })
+              // picUrl.push(shortcodeMedia.edge_sidecar_to_children.edges[index].node.display_resources[1].src)
+            }
+          } else {
+            that.setData({
+              needShowSwipe: false,
+              needShowVideo: true
+            })
+            dataUrls.push({
+              "src": shortcodeMedia.display_resources[1].src,
+              "isVideo": shortcodeMedia.is_video
+
+            })
+            // picUrl.push(shortcodeMedia.display_resources[1].src)
+          }
+          // picUrl = e.data.imgsBase64;
+          isVideo = shortcodeMedia.is_video
+
+          console.log(dataUrls)
           that.setData({
             isHidePic: false,
-            avatarUrl: e.data.avatar_url,
-            avatarName: e.data.avatar_name,
+            avatarUrl: avatarUrl,
+            avatarName: avatarName,
             // picUrls: picUrl
             // picUrl: picUrl[0]
             // imgList: picUrl
-            isVideo: e.data.isVideo
+            isVideo: isVideo,
+            isShowSave: true,
+            saveTip: '保存到相册',
+            dataUrls: dataUrls
           })
-          if (e.data.isVideo) {
-            that.setData({
-              videoUrl: e.data.videoUrl
-            })
-            // wx.downloadFile({
-            //   url: e.data.videoUrl,
-            //   success: function(res) {
-            //     if (res.statusCode === 200) {
-            //       wx.saveVideoToPhotosAlbum({
-            //         filePath: res.tempFilePath,
-            //         success(res) {
-            //           console.log(res)
-            //           wx.showToast({
-            //             title: '保存成功',
-            //           })
-            //         }
-            //       })
-            //     }
-            //   }
-            // })
+          // if (isVideo) {
+          //   videoUrl = shortcodeMedia.video_url
+          //   that.setData({
+          //     videoUrl: videoUrl
+          //   })
 
-          } else {
-            that.setData({
-              imgList: picUrl
-            })
-          }
+
+          // } else {
+          //   that.setData({
+          //     imgList: picUrl
+          //   })
+          // }
         }
       },
       complete: function() {
         wx.hideLoading()
       }
     })
+  },
+
+  saveToAblum: function(e) {
+    wx.showLoading({
+      title: '保存中...',
+    })
+    var that = this
+    for (var index = 0; index < dataUrls.length; index++) {
+      var isVideo = dataUrls[index].isVideo
+      var url = dataUrls[index].src
+      if (isVideo) {
+        wx.downloadFile({
+          url: url,
+          success: function(res) {
+            if (res.statusCode === 200) {
+              wx.saveVideoToPhotosAlbum({
+                filePath: res.tempFilePath,
+                success(res) {
+                  console.log(res)
+                  saveTip = ' 保存成功'
+                  that.setData({
+                    saveTip: saveTip
+                  })
+                  isShowSave = false
+                  setTimeout(
+                    function() {
+                      that.setData({
+                        isShowSave: isShowSave
+                      })
+                    },
+                    1000
+                  )
+                }
+              })
+            }
+          },
+          complete: function() {
+            wx.hideLoading()
+          }
+        })
+      } else {
+        wx.getImageInfo({
+          src: url,
+          success: function(res) {
+            wx.saveImageToPhotosAlbum({
+              filePath: res.path,
+              success: function(res) {
+                saveTip = '保存成功'
+                that.setData({
+                  saveTip: saveTip
+                })
+                isShowSave = false
+                setTimeout(
+                  function() {
+                    that.setData({
+                      isShowSave: isShowSave
+                    })
+                  },
+                  1000
+                )
+              }
+            })
+          },
+          complete: function() {
+            wx.hideLoading()
+          }
+        })
+      }
+    }
   },
 
   getPic: function(e) {
@@ -231,5 +354,17 @@ Page({
     this.setData({
       current: e.detail.current
     })
+  },
+
+  onShareAppMessage: function(res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '给你分享一张图片',
+      path: '/pages/home/home?inputUrl=' + inputUrl,
+      imageUrl: dataUrls[0]
+    }
   }
 })
